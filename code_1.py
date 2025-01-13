3cba85653310928b97a8b5ca5b7a59b0508a4f5c
DorianSaa <astorg.dorian@icloud.com>
2025-01-13 08:22:48 +0000

    mise en route

---
+++ code_1.py
@@ -0,0 +1,18 @@
+joueur1='jaune'
+joueur2='rouge'
+
+def grille():
+    grille=[
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0]
+    ]
+def joueur_actif(joueur):
+    if joueur=joueur1:
+        joueur=joueur2
+    else
+
+
