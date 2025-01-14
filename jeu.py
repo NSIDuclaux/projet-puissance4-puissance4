4322264af7332def93b7b6dd735f894dfbe39283
DorianSaa <astorg.dorian@icloud.com>
2025-01-14 11:10:02 +0100

    derniere modif

--- jeu.py
+++ jeu.py
@@ -28,16 +28,19 @@ class Puissance4():
         
 
     def verif_colone(self,grille):
+        print("verif_col")
         for k in range(6):
             if grille[k][0]==grille[k][1] and grille[k][1]==grille[k][2] and grille[k][2]==grille[k][3] and grille[k][3]==grille[k][4]:
                 return "Victoire"
             elif grille[k][1]==grille[k][2] and grille[k][2]==grille[k][3] and grille[k][3]==grille[k][4] and grille[k][4]==grille[k][5]:
                 return "Vicotire"
-            elif self.grille[k][5]==self.grille[k][6] and self.grille[k][2]==self.grille[k][3] and self.grille[k][3]==self.grille[k][4] and self.grille[k][4]==self.grille[k][5]:
+            elif grille[k][5]==grille[k][6] and grille[k][2]==grille[k][3] and grille[k][3]==grille[k][4] and grille[k][4]==grille[k][5]:
                 return "Vicotire"
-            
-        return self.verif_ligne(self.grille)
+        print("fin")        
+        self.verif_ligne(self.grille)
+    
     def verif_ligne(self,grille):
+        print("verif_lin")
         for k in range(6):
             if grille[0][k]==grille[1][k] and grille[1][k]==grille[2][k] and grille[2][k]==grille[3][k] and grille[3][k]==grille[4][k]:
                 return "Victoire"
@@ -45,15 +48,15 @@ class Puissance4():
                 return "Vicotire"
             elif grille[5][k]==grille[6][k] and grille[2][k]==grille[3][k] and grille[3][k]==grille[4][k] and grille[4][k]==grille[5][k]:
                 return "Vicotire"
+        self.jeu()
         
-        return self.jeu()
     
     def hauteur(self,grille,colone):
         for k in range (len(grille)):
             if grille[colone][k]==0:
                 return k
+    
     def afficher(self,grille):
-        
         print(grille)
         
 
